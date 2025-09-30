fa74eaf0c95f3b7a95ff33e8d915b4ecf5c876a3
XiaqiongZhou-NOAA <48254930+XiaqiongZhou-NOAA@users.noreply.github.com>
2025-09-30 14:35:10 -0400

    Update gw.sh

--- gw.sh
+++ gw.sh
@@ -9,21 +9,13 @@ YAML=./yamls/C192mx025_S2S_REPLAY_ICS.yaml
 export HPC_ACCOUNT=fv3-cpu
 
 ########################
-# for hera
+# for hera/ursa
 export TOPICDIR=/scratch4/NCEPDEV/global/Yangxing.Zheng/ICs/
-# for gaea
-
 export RUNTESTS=/scratch4/NCEPDEV/stmp/$USER
-
 ########################
-# Check Code
-[[ ! -d ${HOMEgfs} ]] && echo "code is not at ${HOMEgfs}" &&  exit 1
-[[ ! -f ${YAML} ]] && echo "yaml file not at ${YAML}" &&  exit 1
-
-echo "HOMEgfs: ${HOMEgfs}"
-echo "YAML: ${YAML}"
+# exp name
 export pslot=c96sfs_test
-
+########################
 source ${HOMEgfs}/dev/ush/gw_setup.sh
 echo $HPC_ACCOUNT
 ${HOMEgfs}/dev/workflow/create_experiment.py -y "${YAML}"
